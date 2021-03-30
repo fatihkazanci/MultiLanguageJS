41dc61fa5f8b1fbef459d0abf171e0b1e51ccc63
Fatih Kazancı <33206545+fatihkazanci@users.noreply.github.com>
2021-03-30 04:08:12 +0300

    Update multilanguage.es5.js

--- multilanguage.es5.js
+++ multilanguage.es5.js
@@ -1,6 +1,6 @@
 /*
 *	MultiLanguageJS
-*	Updated On: 07/01/2019
+*	Updated On: 29/03/2021
 *	By Fatih KAZANCI
 *   Licenced By GNU General Public License v3.0
 *   https://github.com/fatihkazanci/MultiLanguageJS/blob/main/LICENSE
